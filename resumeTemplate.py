7b9a7107fb16d4849e00361a9e0ed040767cc0c4
Damitha Jeanando <damithadj@gmail.com>
2019-07-16 02:03:01 +0530

    Preprocess //

---
+++ resumeTemplate.py
@@ -0,0 +1,21 @@
+from string import Template
+
+#open file
+templatein = open( 'Template.txt' )
+
+#read file
+src = Template(templatein.read())
+
+#data
+name = "hihdic"
+address = "lkmkkl"
+phone = "kmkvlmf"
+email = "l,vlfvfkv"
+experience = ['ewdwe','edwedew']
+education_qualification = ['dedwde','dewdwed']
+d = {'name':name, 'address':address, 'phone':phone, 'email':email, 'experience':'\n'.join(experience),'education_qualification':'\n'.join(education_qualification)}
+
+#substitution
+result = src.substitute(d)
+
+print (result)
